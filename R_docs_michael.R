7f76398dcf13c4ba21d44af7a4fc07e8fb1ed0f0
mre011 <michael.eubanks@cethcpa.com>
2015-07-28 00:51:59 -0500

    Open Intro

--- R_docs_michael.R
+++ R_docs_michael.R
@@ -95,7 +95,8 @@ columnmean <- function(y, removeNA = TRUE) {
 ### "..." arguement indicates a variable number of arguements that are usually passed on to other functions
 
 
-#### Webscraping Basics 
+#### Webscraping Basics
+
 con = url("http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en")
 htmlCode = readLines(con)
 close(con)
@@ -111,8 +112,3 @@ library(httr); html2 = GET(url)
 content2 = content(html2,as="text")
 parsedHtml = htmlParse(content2,asText=TRUE)
 xpathSApply(parsedHtml, "//title", xmlValue)
- 
-#### Basic authentication protocols
-pg2 = GET("http://httpbin.org/basic-auth/user/passwd",
-          authenticate("user","passwd"))
-pg2
